3964c275298e4aee9e8f35a143abec9b5790c5f0
Kamlesh DjKammu <dhundharakamlesh@gmail.com>
2020-04-11 23:20:39 +0530

    Wip

--- database/migrations/2020_04_10_052205_create_course_user_table.php
+++ database/migrations/2020_04_10_052205_create_course_user_table.php
@@ -16,10 +16,9 @@ class CreateCourseUserTable extends Migration
         Schema::create('course_user', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedBigInteger('course_id');
-            $table->foreign('course_id')->references('id')->on('courses');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
-
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
